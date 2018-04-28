472a7389c5f7977f565c1e264fe95a9f8c04a1fa
Lucas Zurverra <lzurverra@gmail.com>
2018-04-28 15:45:01 -0300

    solved problems in execution

--- rgb_led.py
+++ rgb_led.py
@@ -5,19 +5,23 @@ import RPi.GPIO as GPIO
 
 GPIO.setmode(GPIO.BOARD)
 
-red = 13
-green = 15
-blue = 16
+red = 33
+green = 35
+blue = 37
 
 GPIO.setup(red, GPIO.OUT)
 GPIO.setup(green, GPIO.OUT)
 GPIO.setup(blue, GPIO.OUT)
 
 try:
-    c = int(raw_input('> RGB: '))
-    GPIO.setup(red, c[0])
-    GPIO.setup(green, c[1])
-    GPIO.setup(blue, c[2])
+    c = raw_input('> RGB: ')
+
+    while True:
+        GPIO.output(red, int(c[0]))
+        GPIO.output(green, int(c[1]))
+        GPIO.output(blue, int(c[2]))
+
+        c = raw_input('> RGB: ')
 
 except KeyboardInterrupt:
     print '\n\n[ Execution aborted ]'
