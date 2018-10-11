5a179496b21030d0727bd89ce200a7e68e204bcb
Lucas Zurverra <lzurverra@gmail.com>
2018-10-11 21:47:34 +0000

    on proccess: arduino & rpi communication

---
+++ arduino-rpi-com/rpi_interface.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+# coding=utf-8
+import RPi.GPIO as GPIO
+import serial
+#from time import sleep
+
+
+GPIO.setmode(GPIO.BOARD) # Physical numeration pins
+# pin = 7
+# GPIO.setup(pin, GPIO.OUT)
+
+try:
+	ser = ser = serial.Serial('/dev/cu.usbmodem1411', 9600)
+	ser.write('foo')
+    while True:
+        None # Write code here
+except KeyboardInterrupt:
+    print '\n\n[ Execution aborted ]'
+except ValueError:
+    print '\n[ Unexpected value ]'
+except IndexError:
+    print '\n[ Index out of range ]'
+except TypeError:
+    print '\n[ Required values: 0 or 1 ]'
+finally:
+    GPIO.cleanup()
